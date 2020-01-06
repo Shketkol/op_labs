199eb657d2276a69dfccd35a5a4301cb580a9912
Your Name <you@example.com>
2020-01-06 21:18:09 +0200

    update

--- Lab5/Lab5/Lab5/Form1.cs
+++ Lab5/Lab5/Lab5/Form1.cs
@@ -35,5 +35,10 @@ namespace Lab5
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- Lab7/Lab7/Lab7/Form1.cs
+++ Lab7/Lab7/Lab7/Form1.cs
@@ -20,12 +20,13 @@ namespace Lab7
         {
             Graphics myGraphics = e.Graphics;
             Pen myPen = new Pen(Color.Blue, 2);
+            Pen myPenRed = new Pen(Color.Red, 3);
             
             //Surnasname
             myGraphics.DrawLine(myPen, 10, 10, 10, 60);
-            myGraphics.DrawLine(myPen, 30, 10, 30, 60);
+            myGraphics.DrawLine(myPenRed, 30, 10, 30, 60);
             myGraphics.DrawLine(myPen, 50, 10, 50, 60);
-            myGraphics.DrawLine(myPen, 10, 60, 50, 60);
+            myGraphics.DrawLine(myPenRed, 10, 60, 50, 60);
 
             myGraphics.DrawLine(myPen, 70, 10, 70, 60);
             myGraphics.DrawLine(myPen, 70, 30, 100, 10);
@@ -34,7 +35,7 @@ namespace Lab7
             myGraphics.DrawLine(myPen, 110, 10, 110, 60);
 
             myGraphics.DrawLine(myPen, 120, 10, 150, 10);
-            myGraphics.DrawLine(myPen, 135, 10, 135, 60);
+            myGraphics.DrawLine(myPenRed, 135, 10, 135, 60);
 
             //Name
             myGraphics.DrawLine(myPen, 180, 10, 180, 60);
@@ -42,7 +43,7 @@ namespace Lab7
             myGraphics.DrawLine(myPen, 200, 30, 220, 10);
             myGraphics.DrawLine(myPen, 220, 10, 220, 60);
 
-            myGraphics.DrawLine(myPen, 230, 10, 230, 60);
+            myGraphics.DrawLine(myPenRed, 230, 10, 230, 60);
             myGraphics.DrawLine(myPen, 230, 60, 250, 10);
             myGraphics.DrawLine(myPen, 250, 10, 250, 60);
 
@@ -62,20 +63,7 @@ namespace Lab7
             myGraphics.DrawLine(myPen, 370, 10, 380, 60);
             myGraphics.DrawLine(myPen, 365, 30, 375, 30);
 
-            //Patromic
-            myGraphics.DrawLine(myPen, 400, 60, 410, 10);
-            myGraphics.DrawLine(myPen, 410, 10, 420, 60);
-            myGraphics.DrawLine(myPen, 405, 30, 415, 30);
-
-            myGraphics.DrawLine(myPen, 430, 10, 430, 10);
-            myGraphics.DrawLine(myPen, 430, 30, 450, 30);
-            myGraphics.DrawLine(myPen, 450, 10, 450, 60);
-
-
             myGraphics.Dispose();
-
-
-
         }
     }
 }
